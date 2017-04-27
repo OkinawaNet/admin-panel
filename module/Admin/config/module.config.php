60f3974a4341b0ca5e544fab182bde139ce51480
Hasan <hasan@localhost.localdomain>
2017-04-28 02:54:20 +0400

    Zend 2, ZfcUser, Doctrine 2 installed

---
+++ module/Admin/config/module.config.php
@@ -0,0 +1,53 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Admin\Controller\Index' => 'Admin\Controller\IndexController',
+        ),
+    ),
+    'doctrine' => array(
+        'driver' => array(
+            // defines an annotation driver with two paths, and names it `my_annotation_driver`
+            'admin_entity' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(
+                    __DIR__ . '/../src/Admin/Entity',
+                ),
+            ),
+
+            // default metadata driver, aggregates all other drivers into a single one.
+            // Override `orm_default` only if you know what you're doing
+            'orm_default' => array(
+                'drivers' => array(
+                    // register `my_annotation_driver` for any entity under namespace `My\Namespace`
+                    'Admin\Entity' => 'admin_entity'
+                )
+            )
+        )
+    ),
+    // The following section is new and should be added to your file
+    'router' => array(
+        'routes' => array(
+            'admin' => array(
+                'type'    => 'literal',
+                'options' => array(
+                    'route'    => '/admin/',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'admin\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'admin' => __DIR__ . '/../view',
+        ),
+    ),
+);
