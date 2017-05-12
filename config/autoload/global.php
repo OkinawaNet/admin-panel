beb3061b02d7894f437d89dcbc02ac78fbf805e1
Hasan <hasan@localhost.localdomain>
2017-05-12 08:59:36 +0400

    zzz

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -2,7 +2,9 @@
 
 return array(
     'scn-social-auth' => array(
-        'google_client_id' => '592105986464-4eps2qoth2pusmka47tptfu3t52dlloe.apps.googleusercontent.com',
-        'google_secret' => 'UhJO6Jw5jGiSc4t2Xzg4hiSW',
+        'google_client_id' => '',
+        'google_secret' => '',
+        'facebook_client_id' => '',
+        'facebook_secret' => '',
     ),
 );
